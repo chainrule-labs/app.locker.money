a48a9f182b9c45538e1257149aa082a6aa3cab6a
0xernesto <78889960+0xernesto@users.noreply.github.com>
2024-04-19 20:28:04 -0500

    feat: improve email appearance and content

--- app/api/webhooks/moralis-tx-stream/route.ts
+++ app/api/webhooks/moralis-tx-stream/route.ts
@@ -182,6 +182,15 @@ export async function POST(request: Request) {
       lockerId: locker.id,
     };
 
+    const supportedChains: { [key: number]: string } = {
+      "84532": "Base Sepolia",
+      "59141": "Linea Sepolia",
+      "421614": "Arbitrum Sepolia",
+      "100": "Gnosis Chain",
+    };
+
+    const chainName = supportedChains[chainId];
+
     const insertedTxs = await db
       .insert(transactions)
       .values(newTx)
@@ -196,11 +205,21 @@ export async function POST(request: Request) {
     const amountStr = `${amount} ${tokenSymbol}`;
     const link = `${process.env.API_HOST}/tx/${insertedTxs[0].insertedTxHash}`;
     const to = user.emailAddresses[0].emailAddress;
+    const emailHTML = `<div style="font-family: Arial, sans-serif; color: #333; text-align: start; background-color: #F7F7F7; padding: 20px;">
+    <div style="max-width: 600px; margin: auto; background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+      <img src="${process.env.API_HOST}/iconLockerTransOvals.svg" alt="Locker Logo" style="width: 100px; height: auto; margin-bottom: 20px;">
+      <h2 style="color: #4C4FE4;">Update on Your Locker Transaction</h2>
+      <p>Your locker with address ${toAddress} just received ${amountStr} on the ${chainName} network.</p>
+      <a href="${link}" style="color: #2AAAD9; text-decoration: none; font-weight: bold; display: inline-block; margin-top: 20px; padding: 10px 15px; background-color: #4C4FE4; border-radius: 4px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">Go to your Locker dashboard</a>
+      <hr style="border-color: #E5E5E5; border-style: solid; border-width: 1px 0 0;">
+      <p style="font-size: small; color: #666;">You received this email because you are registered with Locker. If you believe this was an error, please <a href="mailto:support@chainrule.io" style="color: #4C4FE4;">contact us</a>.</p>
+    </div>
+  </div>`;
     await resend.emails.send({
       from: "Locker <contact@noreply.locker.money>",
       to,
       subject: `Received ${amountStr} in Locker`,
-      html: `<div><h1>Great news!</h1><br/><p>Your locker with address ${toAddress} just received ${amountStr}.</p><br/><a href=${link}>See more!</a></div>`,
+      html: emailHTML,
     });
 
     console.log("Email sent to " + to);
